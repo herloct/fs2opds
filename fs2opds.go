f8c0f41dbb93ab64575ec83b66bcdcea4697f248
herloct <herloct@gmail.com>
2025-01-07 18:11:17 +0000

    Implements graceful shutdown

--- fs2opds.go
+++ fs2opds.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
+	// Setup
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.GET("/healthcheck", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Ok")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	// Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
