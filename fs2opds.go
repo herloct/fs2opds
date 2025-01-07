1b8a4a7400efb16facddf3a0fb658b498c0c35d3
herloct <herloct@gmail.com>
2025-01-07 18:06:17 +0000

    Add Go Echo

--- fs2opds.go
+++ fs2opds.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/healthcheck", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Ok")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
